01aa2357294633c200cf8991f893954062676d4d
Dustin Rohde <xdrohdex@gmail.com>
2016-02-20 17:06:18 -0500

    Added support for blank spots on the world grid

--- src/ecosystem.js
+++ src/ecosystem.js
@@ -6,17 +6,16 @@ const things = require('./things');
 const ecosystem = World.fromLegend(
     new Map([
         ['#', things.Wall],
-        ['.', things.Floor],
         ['*', things.Plant],
         ['H', things.Herbivore]
     ]),
     [
         '#######'.split(''),
-        '#.*...#'.split(''),
-        '#.*...#'.split(''),
-        '#..*..#'.split(''),
-        '#..H..#'.split(''),
-        '#.....#'.split(''),
+        '# *   #'.split(''),
+        '# *   #'.split(''),
+        '#  *  #'.split(''),
+        '#  H  #'.split(''),
+        '#     #'.split(''),
         '#######'.split('')
     ]
 );
