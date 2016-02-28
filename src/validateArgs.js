7f3b2bdfd191b147ce167b4fd52446b0cd69e871
Dustin Rohde <xdrohdex@gmail.com>
2016-02-27 19:20:13 -0500

    Removed validateArgs module as it was no longer in use

--- src/validateArgs.js
+++
@@ -1,7 +0,0 @@
-function validate(params, expected) {
-    if (!expected.every(p => p in params)) 
-        throw new Error(
-            `Missing one or more parameters: ${expected.join(', ')}`)
-}
-
-export { validate as default }
