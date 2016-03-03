b10c80c665d41e5623e054b7509aca8e24fda50e
Dustin Rohde <xdrohdex@gmail.com>
2016-03-02 22:16:23 -0500

    Updated eslint and changed eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
-{
+module.exports = {
     "env": {
         "browser": true,
-        "commonjs": true,
-        "es6": true
+        "es6": true,
+        "node": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
@@ -26,7 +26,10 @@
         ],
         "block-scoped-var": 2,
         "block-spacing": 2,
-        "brace-style": 2,
+        "brace-style": [
+            2,
+            "1tbs"
+        ],
         "callback-return": 2,
         "camelcase": 2,
         "comma-dangle": [
@@ -53,14 +56,20 @@
         "consistent-this": 2,
         "curly": 0,
         "default-case": 2,
-        "dot-location": 2,
+        "dot-location": [
+            2,
+            "object"
+        ],
         "dot-notation": 2,
         "eol-last": 2,
         "eqeqeq": 2,
         "func-names": 2,
         "func-style": [
-            0,
-            "declaration"
+            2,
+            "declaration",
+            {
+                "allowArrowFunctions": true
+            }
         ],
         "generator-star-spacing": 2,
         "global-require": 2,
@@ -69,22 +78,30 @@
         "id-blacklist": 2,
         "id-length": 0,
         "id-match": 2,
-        "indent": 2,
+        "indent": [
+            2,
+            2
+        ],
         "init-declarations": 2,
         "jsx-quotes": 2,
         "key-spacing": 2,
-        "keyword-spacing": 2,
+        "keyword-spacing": [
+            2,
+            {
+                "after": true,
+                "before": true
+            }
+        ],
         "linebreak-style": [
             2,
             "unix"
         ],
         "lines-around-comment": 2,
         "max-depth": 2,
-        "max-len": 2,
+        "max-len": 0,
         "max-nested-callbacks": 2,
         "max-params": 2,
         "max-statements": 2,
-        "new-cap": 0,
         "new-parens": 2,
         "newline-after-var": 0,
         "newline-per-chained-call": 0,
@@ -94,13 +111,12 @@
         "no-caller": 2,
         "no-catch-shadow": 2,
         "no-confusing-arrow": 0,
-        "no-console": 1,
         "no-continue": 2,
         "no-div-regex": 2,
         "no-else-return": 2,
         "no-empty-function": 2,
         "no-eq-null": 2,
-        "no-eval": 2,
+        "no-eval": 0,
         "no-extend-native": 2,
         "no-extra-bind": 2,
         "no-extra-label": 2,
@@ -110,7 +126,7 @@
         "no-implicit-globals": 2,
         "no-implied-eval": 2,
         "no-inline-comments": 2,
-        "no-invalid-this": 1,
+        "no-invalid-this": 2,
         "no-iterator": 2,
         "no-label-var": 2,
         "no-labels": 2,
@@ -133,7 +149,7 @@
         "no-octal-escape": 2,
         "no-param-reassign": 2,
         "no-path-concat": 2,
-        "no-plusplus": 0,
+        "no-plusplus": 2,
         "no-process-env": 2,
         "no-process-exit": 2,
         "no-proto": 2,
@@ -144,7 +160,7 @@
         "no-script-url": 2,
         "no-self-compare": 2,
         "no-sequences": 2,
-        "no-shadow": 0,
+        "no-shadow": 2,
         "no-shadow-restricted-names": 2,
         "no-spaced-func": 2,
         "no-sync": 2,
@@ -157,7 +173,6 @@
         "no-unmodified-loop-condition": 2,
         "no-unneeded-ternary": 2,
         "no-unused-expressions": 2,
-        "no-unused-vars": 1,
         "no-use-before-define": 0,
         "no-useless-call": 2,
         "no-useless-concat": 2,
@@ -174,7 +189,10 @@
         "object-shorthand": 2,
         "one-var": 0,
         "one-var-declaration-per-line": 2,
-        "operator-assignment": 2,
+        "operator-assignment": [
+            2,
+            "always"
+        ],
         "operator-linebreak": 2,
         "padded-blocks": [
             2,
@@ -182,10 +200,7 @@
         ],
         "prefer-arrow-callback": 2,
         "prefer-const": 2,
-        "prefer-reflect": [
-            2,
-            { "exceptions": ["delete"] }
-        ],
+        "prefer-reflect": 2,
         "prefer-rest-params": 2,
         "prefer-spread": 2,
         "prefer-template": 2,
@@ -210,16 +225,19 @@
         "space-infix-ops": 2,
         "space-unary-ops": 2,
         "spaced-comment": 2,
-        "strict": [
+        "strict": 2,
+        "template-curly-spacing": [
             2,
             "never"
         ],
-        "template-curly-spacing": 2,
         "valid-jsdoc": 2,
         "vars-on-top": 2,
         "wrap-iife": 2,
         "wrap-regex": 2,
         "yield-star-spacing": 2,
-        "yoda": 2
+        "yoda": [
+            2,
+            "never"
+        ]
     }
-}
+};
