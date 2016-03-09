808767a29196f26ba132b072aba3cd6e013f18ab
Dustin Rohde <xdrohdex@gmail.com>
2016-03-09 16:45:32 -0500

    Begun transition to jade

--- server/index.js
+++ server/index.js
@@ -1,50 +1,31 @@
-import fs from 'fs'
 import path from 'path'
 
 import express from 'express'
-import markdownRouter from 'express-markdown-router'
+import jade from 'jade'
 import multer from 'multer'
-import consolidate from 'consolidate'
 
 const app = express()
 
-const storage = multer.memoryStorage()
-const upload = multer({ storage })
-
 app.set('port', process.env.PORT || 5000)
-
-// Markdown middleware
-app.use(markdownRouter(path.join(__dirname, 'views')))
 
 // Static file server
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/scripts',
   express.static(path.join(__dirname, '/node_modules/babel-polyfill/dist/')))
 
-// Lodash templates
-app.engine('html', consolidate.lodash)
-app.set('view engine', 'html')
+app.set('view engine', 'jade')
 app.set('views', path.join(__dirname, '/views'))
 
-// Index
 app.get('/', (request, response) => {
   response.render('index')
 })
 
-// App page
-app.get('/app', (request, response) => {
-  response.render('app')
+app.get('/play', (request, response) => {
+  response.render('play')
 })
 
-// config file upload
-app.post('/app', upload.single('configFile'), (request, response) => {
-  const data = request.file.toString('utf-8')
-  response.redirect('app')
-})
-
-// Example config
 app.get('/example', (request, response) => {
-  response.set('Content-Type', 'application/json')
+  response.set('Content-Type', 'text/yaml')
   response.download(path.join('public', 'example.yml'))
 })
 
