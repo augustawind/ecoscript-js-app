fd0eb458f7d4013ed8b2a20d063a3998c511f743
Dustin Rohde <xdrohdex@gmail.com>
2016-03-09 19:36:17 -0500

    Cleaned up main.js a lot

--- client/main.js
+++ client/main.js
@@ -1,10 +1,11 @@
 import 'babel-polyfill'
+import ajax from 'ajax'
 import yaml from 'js-yaml'
 
-import parseConfig from './configParser'
+import parseWorld from './configParser'
 
-const EXAMPLE_URL = 'example.yml'
-const TIMEOUT = 450
+const exampleURL = 'example.yml'
+const interval = 450
 
 function animateWorld(world, canvas) {
   const step = () => {
@@ -12,42 +13,22 @@ function animateWorld(world, canvas) {
     world.turn()
   }
 
-  return window.setInterval(step, TIMEOUT)
+  return window.setInterval(step, interval)
 }
 
 window.onload = () => {
-  const doc = window.document
-  const canvas = doc.getElementById('ecosystem')
-  if (!canvas) return
-
-  let animation
-
-  const xhr = new XMLHttpRequest()
-
-  xhr.onload = () => {
-    const config = yaml.safeLoad(xhr.responseText)
-    const world = parseConfig(config)
-    animation = animateWorld(world, canvas)
-  }
-
-  xhr.onerror = () => {
-    throw new Error(`Failed to load ${EXAMPLE_URL}: ${xhr.status}`)
-  }
-
-  xhr.open('get', EXAMPLE_URL, true)
-  xhr.send()
-
-  doc.getElementById('submit-file').onclick = () => {
-    const reader = new FileReader()
-    reader.onload = event => {
-      const contents = event.target.result
-      const config = yaml.safeLoad(contents)
-      const world = parseConfig(config)
-      clearInterval(animation)
-      animation = animateWorld(world, canvas)
+  const settings = {
+    error: (xhr, status, exception) => {
+      console.log(status)
     }
-
-    const file = doc.getElementById('select-file').files[0]
-    reader.readAsText(file)
   }
+
+  const callback = data => {
+    const config = yaml.safeLoad(data)
+    const world = parseWorld(config)
+    const canvas = window.document.getElementById('ecosystem')
+    animateWorld(world, canvas)
+  }
+
+  ajax.get(exampleURL, settings, callback)
 }
